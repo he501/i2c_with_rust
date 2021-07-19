9041e4e941072839076bdfe079bdf426a419335f
he501 <81799557+he501@users.noreply.github.com>
2021-07-19 18:37:39 +0000

    resistance value can be changed manually

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use rppal::i2c::I2c;
+
+const ADDR_I2C: u16 = 0x2F;
+const VALUE: u8 = 5;
+
+fn acquire() -> Result<u8, rppal::i2c::Error> {
+    let mut i2c = I2c::new()?;
+    i2c.set_slave_address(ADDR_I2C)?;
+    i2c.write(&[VALUE])?;
+    Ok(0)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _result = acquire();
 }
